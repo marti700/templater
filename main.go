cde0ecf5f4b9021fa271e11b6227ce1529a888e8
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2024-05-13 06:54:27 -0400

    substituted place holders with input type texts in html

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"regexp"
 	"text/template"
 
 	"code.sajari.com/docconv/v2"
@@ -13,13 +14,45 @@ type DocMetadata struct {
 	Document string
 }
 
+func replacePlaceholders(str string) string {
+	// Define a regular expression to match placeholders
+	pattern := `{([^{}]+):([^}]+)}`
+	re := regexp.MustCompile(pattern)
+
+	// Replace function to create the HTML input element
+	replace := func(match string) string {
+		placehoder := re.FindStringSubmatch(match)[1:] // Capture name and value from matched group
+		return fmt.Sprintf("<input type=\"text\" name='%s' placeholder='%s'></input>", placehoder[0], placehoder[0])
+	}
+
+	// Substitute all placeholders with the replace function
+	return re.ReplaceAllStringFunc(str, replace)
+}
+
+func replaceEmptyLines(text string) string {
+	// Define a regular expression that matches one or more empty lines
+	pattern := `\n{2,}`
+	re := regexp.MustCompile(pattern)
+
+	// Replace function to insert a paragraph tag
+	replace := func(match string) string {
+		return fmt.Sprintf("</p>%s<p>", match)
+	}
+
+	// Substitute empty lines with the replace function
+	return re.ReplaceAllStringFunc(text, replace)
+}
+
 func main() {
 	res, err := docconv.ConvertPath("Acto de Venta Alfredo Mateo.docx")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	fmt.Println(res.Body)
+
+	// inputText := template.Must(template.New("inputText").Parse(`<input type="text" value='' placehoder=''></input>`))
 	metadata := DocMetadata{
-		Document: res.Body,
+		Document: replaceEmptyLines(replacePlaceholders(res.Body)),
 	}
 
 	http.HandleFunc("/document", func(w http.ResponseWriter, r *http.Request) {
